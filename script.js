879a061d8e2f716ea4074c3e68965252f125a1b4
Ashwin Muthu Kumar S <67495595+Ashwin1950@users.noreply.github.com>
2021-04-28 04:42:40 +0530

    updated UI

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-var separate,
+var magazineWords,
   word,
   flag1 = 0,
   notAvail = [];
 const disp1 = document.getElementById("disp1");
 const disp2 = document.getElementById("disp2");
 
+//splitting the magazine text into separate words
 function splitMagazine(str) {
   if (!str) {
     alert("Invalid!!! Don't leave the boxes empty!");
@@ -12,9 +13,10 @@ function splitMagazine(str) {
     disp2.style.display = "none";
     exit;
   }
-  separate = str.split(" ");
+  magazineWords = str.split(" ");
 }
 
+//splitting the input text into separate words
 function splitWords(str) {
   if (!str) {
     alert("Invalid!!! Don't leave the boxes empty!");
@@ -27,57 +29,58 @@ function splitWords(str) {
 
 function takeIn() {
   var str = document.getElementById("magazine").value;
-  // console.log(str.replace(/[\!\@\#\$\%\^\&\*\)\(\+\=\.\<\>\{\}\[\]\:\;\'\"\|\~\`\_\-\\\/\,\?]/g, ' '));
-  str = str.replace(
-    /[\!\@\#\$\%\^\&\*\)\(\+\=\.\<\>\{\}\[\]\:\;\'\"\|\~\`\_\-\\\/\,\?]/g,
-    " "
-  );
+  //replacing all the special characters with space
+  str = str.replace(/[\!\@\#\$\%\^\&\*\)\(\+\=\.\<\>\{\}\[\]\:\;\'\"\|\~\`\_\-\\\/\,\?]/g," "); 
   splitMagazine(str);
 }
 
-function containsValue() {
+function callFunction() {
   takeIn();
   notAvail.splice(0, notAvail.length);
   var str = document.getElementById("words").value;
   str = str.replace("  ", " ");
+  str = str.replace("   ", " ");
   //   console.log(str);
   splitWords(str);
   var i;
   for (i = 0; i < word.length; i++) {
-    compareBoth(word[i]);
+    compareBoth(word[i]);  
   }
 }
 
 function compareBoth(wrd) {
   var i,
     flag = 0;
-  for (i = 0; i < separate.length; i++) {
-    if (separate[i].localeCompare(wrd) == 0) {
+  //comparing each word left by kidnapper with that in the magazine
+  for (i = 0; i < magazineWords.length; i++) {
+    if (magazineWords[i].localeCompare(wrd) == 0) { 
       flag = 1;
       break;
     }
   }
   if (flag != 1) {
-    notAvail.push(wrd);
+    notAvail.push(wrd);  //pushing the unexisting words into notAvail
   }
   disp1.style.display = "none";
   disp2.style.display = "none";
+
+  //displaying the final result
   if (notAvail.length == 0) {
     disp1.style.display = "block";
     document.getElementById("demopos1").innerHTML =
-      "Yes. All the words are available in this Magazine.";
+      "All the words exist in this Magazine.";
     document.getElementById("demopos2").innerHTML =
-      "The Kidnapper has taken all the ransom words from this Magazine only.";
+      "Yes. The Kidnapper has taken all the ransom words from this Magazine only.";
   } else {
     disp2.style.display = "block";
     if (notAvail.length == 1) {
       document.getElementById("demoneg1").innerHTML =
-        'No. The word "' + notAvail + '" is not taken from the Magazine.';
+        (notAvail.length)+' word "' + notAvail + '" does not exist in this Magazine.';
     } else {
       document.getElementById("demoneg1").innerHTML =
-        'No. The words "' + notAvail + '" are not taken from the Magazine.';
+        (notAvail.length)+' words "' + notAvail + '" do not exist in this Magazine.';
     }
     document.getElementById("demoneg2").innerHTML =
-      "The Kidnapper has not taken the words from this Magazine.";
+      "No. The Kidnapper has not taken the words from this Magazine.";
   }
 }
